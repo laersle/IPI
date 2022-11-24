2523b7cf2adcd8feebcad780e533162d7824fc04
laersle <larsweber22@web.de>
2022-11-24 22:23:19 +0100

    finalize uebung 3

--- uebung03/binomial.cpp
+++ uebung03/binomial.cpp
@@ -5,11 +5,6 @@ int binRec(int n,int k){
                 cond(n == k, 1,binRec(n-1, k-1) + binRec(n-1, k)));
 }
 
-void printint(int a, int b){
-    print(a);
-    print(b);
-}
-
 int main(int argc, char** argv){
     print(binRec(readarg_int(argc, argv, 1), readarg_int(argc, argv,2)));
     return 0;
