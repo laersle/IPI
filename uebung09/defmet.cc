57a339cf6c3d5f074dbe15a1fbc1e5e1147a0f51
Lars <larsweber22@web.de>
2023-01-13 20:49:31 +0100

    update B9/A1

--- uebung09/defmet.cc
+++ uebung09/defmet.cc
@@ -25,40 +25,46 @@ Zahl::Zahl() { z = 0;
     std::cout << this << ":    Default Konstruktor" << std::endl;
 }
 Zahl::Zahl( const int& n )  { z = n;
-    std::cout << "Int Konstruktor" << std::endl;
+    std::cout << this << ":    Int Konstruktor  n=" << n << std::endl;
 }
 Zahl::Zahl( const Zahl& n ) { z = n.z;
-    std::cout << "Copy Konstruktor" << std::endl;
+    std::cout << this << ":    Copy Konstruktor | copy of: " << &n << std::endl;
 }
 Zahl::~Zahl() {
-    std::cout << "Destruktor" << std::endl;
+    std::cout << this << ":    Destruktor" << std::endl;
 }
 Zahl& Zahl::operator=( const int& n )
 {
     z=n;
-    std::cout << "Zuweisung (int-ref)" << std::endl;
+    std::cout << this << ":    Zuweisung (int-ref)  n=" << n  << std::endl;
     return *this;
 }
 Zahl& Zahl::operator=( const Zahl& n )
 {
     z=n.z;
-    std::cout << "Zuweisung (Obj-ref)" << std::endl;
+    std::cout << this << ":    Zuweisung (Obj-ref)  obj=" << &n << std::endl;
     return *this; }
 
 Zahl Zahl::operator+( const Zahl& n )
 {
-    std::cout << "Addition" << std::endl;
+    std::cout << this << ":    Addition | add: " << &n << std::endl;
     return Zahl(z+n.z); }
 
 Zahl f( Zahl a, Zahl& b) { return a+b; }
 
 int main()
 {
+  std::cout << "Zeile 37" << std::endl;
   Zahl a(3), b(4);
+  std::cout << "Zeile 38" << std::endl;
   Zahl c(a), d, e;
+  std::cout << "Zeile 39" << std::endl;
   d = 5;
+  std::cout << "Zeile 40" << std::endl;
   d = f(d,c);
+  std::cout << "Zeile 41" << std::endl;
   e = d + a + b;
+  std::cout << "Zeile 42" << std::endl;
   return 0;
 }
 /* @VERBOFF */
